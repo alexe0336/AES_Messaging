009f318aee8359705db004ff0cd2e3c45d982c34
Alex <alexe0336@gmail.com>
2024-03-31 00:23:37 -0400

    comments changed

--- receiver.py
+++ receiver.py
@@ -63,9 +63,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     sender_signed_DH_public_key = client_socket.recv(1024)  # Receive sender.py's RSA public key
     print("test2")
     rsa_public_key_bytes = client_socket.recv(1024)  # Receive sender.py's signed DH public key
-    print("Received sender.py's RSA public key:", rsa_public_key_bytes)
+    print("test3")
     sender_DH_public_key_bytes = client_socket.recv(1024)  # Receive sender.py's DH public key
-    print("Received sender.py's public key:", sender_DH_public_key_bytes)
+    print("test4")
 
     # Convert the RSA public key from bytes back to an RSA public key object
     # Convert bytes back to an RSA public key object
