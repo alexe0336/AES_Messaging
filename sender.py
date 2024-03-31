221d8fe09731241abbe96ebe08a07537a6b826ea
Alex <alexe0336@gmail.com>
2024-03-30 23:31:33 -0400

    Update sender.py with necessary imports and remove unused code

--- sender.py
+++ sender.py
@@ -3,12 +3,13 @@
 
 #import the necessary libraries
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography import x509
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
-import os
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+
     # Libraries for TCP socket API
 import socket
 from cryptography.hazmat.primitives import serialization, hashes
